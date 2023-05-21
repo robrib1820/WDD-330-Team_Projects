bfe6522a85ad91b28491f743f6fc30e1c2a9c0ea
Sebastiao Matusse <matussevalete@gmail.com>
2023-05-21 15:28:55 -0600

    add to cart handler working just fine

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,8 @@
 // import { setLocalStorage, getParam } from "./utils.mjs";
-// import { findProductById } from "./productData.mjs";
-
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
+import { findProductById } from "./productData.mjs";
+import { addProductToCart } from "./productDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 const productId = getParam("product");
@@ -10,16 +10,16 @@ productDetails(productId);
 
 loadHeaderFooter();
 
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
 
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
 
 // const productId = getParam("product");
 

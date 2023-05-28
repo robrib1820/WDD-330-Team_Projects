7279a2002f67a8ef4d282781e1afdac290f86db0
Sebastiao Matusse <matussevalete@gmail.com>
2023-05-28 03:37:54 +0200

    removed unsed code

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,3 @@
-import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-
-let selector = document.querySelector(".product-list");
-// selector.appendChild(productList("tents"));
-
-productList(selector, "tents");
 
 loadHeaderFooter();
